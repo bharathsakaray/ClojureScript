b34c1355f17e1146c89457b13b16d6cfe757be0f
technob <bharathkumar.sakaray@technoidentity.com>
2015-08-24 15:58:28 +0530

    Fibinocci using recursion

--- Clojure_Tasks/Fibonacci.clj
+++ Clojure_Tasks/Fibonacci.clj
@@ -1,3 +1,13 @@
-(def fibonacci
-  (map first (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
-(nth fibonacci 2)
+                                        ; CIDER 0.10.0snapshot (package: 20150719.2231) (Java 1.8.0_45, Clojure 1.6.0, nREPL 0.2.6)
+(defn fib2 [n]
+  (condp = n
+    0 0
+    1 1
+    (+ (fib2 (- n 1)) (fib2 (- n 2)))
+    ))
+
+
+
+
+
+
